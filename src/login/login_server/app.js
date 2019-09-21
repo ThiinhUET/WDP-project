6f2b56a720e5f69f554fc6c040a31a7300f02ed4
Nguyễn Tuấn Quốc <quoca2cvp@gmail.com>
2019-09-21 22:25:42 +0700

    .

--- src/login/login_server/app.js
+++ src/login/login_server/app.js
@@ -4,7 +4,7 @@ var http = require('http');
 http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/html'});
 
-  var file = fs.createReadStream('./login.html');
+  var file = fs.createReadStream('../login.html');
   file.pipe(response);
 
 }).listen(8080);
