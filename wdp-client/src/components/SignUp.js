58a01d50e0886266a015dc481759d4e8f8b397b8
ThiinhUET <thinhlevan2015@gmail.com>
2019-10-14 15:22:46 +0700

    Add signin signup event

--- wdp-client/src/components/SignUp.js
+++ wdp-client/src/components/SignUp.js
@@ -3,7 +3,7 @@ import React , {Component} from 'react';
 class SignUp extends Component {
     render() { 
         return (
-            <h1>This is Sign Up Page</h1>
+            <h1  style={{ background: 'white' }}>This is Sign Up Page</h1>
         );
     }
 }
