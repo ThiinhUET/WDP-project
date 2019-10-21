c1a9e5e317cdf3763ff945c2672968b795c40ba2
Nguyễn Tuấn Quốc <quoca2cvp@gmail.com>
2019-10-21 19:27:41 +0700

    update menu bar

--- wdp-client/src/editor/MenuBar.js
+++ wdp-client/src/editor/MenuBar.js
@@ -6,66 +6,73 @@ import './css/menubar.css'
 class MenuBar extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            activeItem: "none"
-        }
-    }
-    setActiveItem = (name) => {
-        this.setState({
-            ...this.state,
-            activeItem: name
-        })
-    }
-    file() {
-        var x = document.getElementById("file_items");
-        if (this.state.activeItem !== "file") {
-            this.setActiveItem("file");
-            x.className += " open";
-            document.getElementById("edit_items").className = "menu_items";
-        }
-        else {
-            this.setActiveItem("none");
-            x.className = "menu_items";
-        }
-    }
-    edit() {
-        var x = document.getElementById("edit_items");
-        if (this.state.activeItem !== "edit") {
-            this.setActiveItem("edit");
-            x.className += " open";
-            document.getElementById("file_items").className = "menu_items";
-        }
-        else {
-            this.setActiveItem("none");
-            x.className = "menu_items";
-        }
     }
     render() {
         return (
             <div className="menubar">
-                <div className="menu_items" id="file_items" onClick={() => this.file()}>
+                <div className="menu_items">
                     File
-                    {this.state.activeItem === "file" && <div className="items_container">
-                        <div className="item_holder 1">New File</div>
-                        <div className="item_holder 1">New Project</div>
-                        <div className="item_holder 1">Open File</div>
-                        <div className="item_holder 1">Open Project</div>
-                        <div className="item_holder 1">Save</div>
-                        <div className="item_holder 1">Export to zip</div>
-                    </div>}
+                    <div className="items_container">
+                        <div className="item_holder">New File</div>
+                        <div className="item_holder">New Project</div>
+                        <div className="item_divider"></div>
+                        <div className="item_holder">Open File</div>
+                        <div className="item_holder">Open Project</div>
+                        <div className="item_divider"></div>
+                        <div className="item_holder">Save</div>
+                        <div className="item_holder">Export to zip</div>
+                    </div>
                 </div>
-                <div className="menu_items" id="edit_items" onClick={() => this.edit()}>
+                <div className="menu_items">
                     Edit
-                    {this.state.activeItem === "edit" && <div className="items_container">
-                        Hello
-                    </div>}
+                    <div className="items_container">
+                        <div className="item_holder">Undo</div>
+                        <div className="item_holder">Redo</div>
+                        <div className="item_divider"></div>
+                        <div className="item_holder">Cut</div>
+                        <div className="item_holder">Copy</div>
+                        <div className="item_divider"></div>
+                        <div className="item_holder">Find</div>
+                        <div className="item_holder">Replace</div>
+                    </div>
                 </div>
-                <div className="menu_items">Selection</div>
-                <div className="menu_items">View</div>
-                <div className="menu_items">Go</div>
-                <div className="menu_items">Debug</div>
-                <div className="menu_items">Terminal</div>
-                <div className="menu_items">Help</div>
+                <div className="menu_items">
+                    Selection
+                    <div className="items_container">
+                        <div className="item_holder">Select All</div>
+                    </div>
+                </div>
+                <div className="menu_items">
+                    View
+                    <div className="items_container">
+                        <div className="item_holder">Command Palette</div>
+                    </div>
+                </div>
+                <div className="menu_items">
+                    Go
+                    <div className="items_container">
+                        <div className="item_holder">
+                            Switch Editor
+                            <span>
+                                <i class="fas fa-angle-right"></i>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className="menu_items">
+                    Terminal
+                    <div className="items_container">
+                        <div className="item_holder">New Terminal</div>
+                        <div className="item_divider"></div>
+                        <div className="item_holder">Open Terminal</div>
+                    </div>
+                </div>
+                <div className="menu_items">
+                    Help
+                    <div className="items_container">
+                        <div className="item_holder">About</div>
+                    </div>
+                </div>
             </div>
         );
     }
