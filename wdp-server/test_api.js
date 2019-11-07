0f1bb1d6095352cb93d456d8f9ef6f136356dcfb
Nguyễn Tuấn Quốc <quoca2cvp@gmail.com>
2019-11-08 01:50:15 +0700

    fix style

---
+++ wdp-server/test_api.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+/*
+axios.post('http://localhost:8080/git/user-info',{
+    uid : 38932674,
+    accessToken: 'b84ae3b95e21bb9f42026031566570b600a5fe43'
+}).then(res =>{
+    console.log(res.data);
+})
+*/
+
+/*
+axios.post('http://localhost:8080/git/user-repos',{
+    login : 'NTQ99',
+    accessToken: 'b84ae3b95e21bb9f42026031566570b600a5fe43'
+}).then(res =>{
+    console.log(res.data);
+})
+*/
+
+/*
+axios.post('http://localhost:8080/git/user-listfile',{
+    login: 'NTQ99',
+    repo : 'Bomberman',
+    accessToken: 'b84ae3b95e21bb9f42026031566570b600a5fe43'
+}).then(res =>{
+    console.log(res.data);
+})
+*/
+
+/*
+axios.post('http://localhost:8080/git/get-file-content',{
+    content: 'https://api.github.com/repos/NTQ99/Bomberman/git/blobs/bb8daad436733fe8b3f4cf2ed400791c30715a72',
+    accessToken: 'b84ae3b95e21bb9f42026031566570b600a5fe43'
+}).then(res =>{
+    console.log(res.data);
+})
+
+*/
