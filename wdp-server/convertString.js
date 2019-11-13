d90c92ccc940dc5636423c2767a9977f5e958e4d
Nguyễn Tuấn Quốc <quoca2cvp@gmail.com>
2019-11-14 00:39:47 +0700

    fix convertString func

--- wdp-server/convertString.js
+++ wdp-server/convertString.js
@@ -22,28 +22,32 @@ class leafNode {
     }
 }
 addNode = (node1, node2) => {
-        if (node1.children.length === 0) node1.addChildren(node2);
-        else {
-            let node3 = node1.children[node1.children.length - 1];
-            if (node2.path.includes(node3.path)) addNode(node3, node2);
-            else node1.addChildren(node2);
-        }
+    if (node1.children.length === 0) node1.addChildren(node2);
+    else {
+        let node3 = node1.children[node1.children.length - 1];
+        if (node2.path.includes(node3.path)) addNode(node3, node2);
+        else node1.addChildren(node2);
+    }
 }
 convertString = (input) => {
-    let arrayInput = input.split('[\n');
-    arrayInput[0] = arrayInput[0].split('"')[9].split('/')[5];
-    let rootNode = new node('', 'tree', arrayInput[0], true, '');
+    // let arrayInput = input.split('[\n');
+    // arrayInput[0] = arrayInput[0].split('"')[9].split('/')[5];
+    let nameNode = input.url.split('/')[5];
+    let rootNode = new node('', 'tree', nameNode, true, '');
     let nodes = [];
-    arrayInput[1] = arrayInput[1].split(']')[0].split('},');
-    for (let i = 0; i < arrayInput[1].length; i ++) {
-        arrayInput[1][i] = arrayInput[1][i].split('"');
-        let pathNode = arrayInput[1][i][3].split('/');
-        nodes[i] = new node(arrayInput[1][i][3], arrayInput[1][i][11], pathNode[pathNode.length - 1], false, (arrayInput[1][i][11] === 'blob')? arrayInput[1][i][21] : '');
+    for (let i = 0; i < input.tree.length; i ++) {
+        let pathNode = input.tree[i].path.split('/');
+        nodes[i] = new node(input.tree[i].path, input.tree[i].type, pathNode[pathNode.length - 1], false, input.tree[i].url);
         if (nodes[i].type === 'blob') nodes[i] = new leafNode(nodes[i]);
         if (nodes[i].type === 'tree' || (nodes[i].type === 'blob' && nodes[i].path.includes('/')))
             addNode(rootNode, nodes[i]);
     }
-    for (let i = 0; i < arrayInput[1].length; i ++) {
+    // arrayInput[1] = arrayInput[1].split(']')[0].split('},');
+    // for (let i = 0; i < arrayInput[1].length; i ++) {
+    //     arrayInput[1][i] = arrayInput[1][i].split('"');
+    //     nodes[i] = new node(arrayInput[1][i][3], arrayInput[1][i][11], pathNode[pathNode.length - 1], false, (arrayInput[1][i][11] === 'blob')? arrayInput[1][i][21] : '');
+    // }
+    for (let i = 0; i < input.tree.length; i ++) {
         if (nodes[i].type === 'blob' && !nodes[i].path.includes('/'))
             addNode(rootNode, nodes[i]);
     }
