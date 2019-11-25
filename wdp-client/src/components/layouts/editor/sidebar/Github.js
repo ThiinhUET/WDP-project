89fb6945ac25ecabcc6de27b4f88c864a7c7facb
ThiinhUET <thinhlevan2015@gmail.com>
2019-11-25 17:37:18 +0700

    heroku

--- wdp-client/src/components/layouts/editor/sidebar/Github.js
+++ wdp-client/src/components/layouts/editor/sidebar/Github.js
@@ -4,6 +4,8 @@ import Authenticate from '../../../common/authprovider/Authenticate';
 import { contentFlow } from '../../../../services';
 import { defaultdata } from '../sidebar/explorer/data';
 import axios from 'axios';
+import 'antd/dist/antd.css';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
 
 class Github extends Component {
     constructor(props) {
@@ -11,15 +13,15 @@ class Github extends Component {
         const location = this.props.location;
         this.state = {
             codeToCommit: "",
-            data: (location.state && location.state.data)? location.state.data : defaultdata,
-            cursor: (location.state && location.state.cursor)? location.state.cursor : '',
+            data: (location.state && location.state.data) ? location.state.data : defaultdata,
+            cursor: (location.state && location.state.cursor) ? location.state.cursor : '',
         }
     }
 
     componentDidMount() {
         this.props.history.listen((location) => this.setState({
-            data: (location.state && location.state.data)? location.state.data : this.state.data,
-            cursor: (location.state && location.state.cursor)? location.state.cursor : this.state.cursor,
+            data: (location.state && location.state.data) ? location.state.data : this.state.data,
+            cursor: (location.state && location.state.cursor) ? location.state.cursor : this.state.cursor,
         }))
     }
 
@@ -27,6 +29,28 @@ class Github extends Component {
         const authenticate = new Authenticate();
         authenticate.signin(() => this.props.history.push('/editor'));
     }
+
+    createNotification = (type) => {
+        return () => {
+          switch (type) {
+            case 'info':
+              NotificationManager.info('Info message');
+              break;
+            case 'success':
+              NotificationManager.success('Success message', 'Commit Success !');
+              break;
+            case 'warning':
+              NotificationManager.warning('Warning message', 'Close after 3000ms', 3000);
+              break;
+            case 'error':
+              NotificationManager.error('Error message', 'Click me!', 5000, () => {
+                alert('callback');
+              });
+              break;
+          }
+        };
+      };
+
     commitCode() {
         this.contenFlowSub = contentFlow.subscribe((value) => {
             let userName = localStorage.getItem('username');
@@ -36,40 +60,43 @@ class Github extends Component {
             let fileName = this.state.cursor.name;
             let message = document.getElementById('commitMsg').value;
             if (value !== '<!--Happy Coding-->') {
-                axios.get('https://api.github.com/repos/' + userName + '/' + project + '/contents/' + fileName, { headers: {'Authorization': 'token ' + accessToken }}).
-                then(res => {
-                    let shaKey = res.data.sha;
-                    let decodedValue = Buffer.from(value).toString('base64');
-                    axios.put('https://api.github.com/repos/' + userName + '/' + project + '/contents/' + fileName, {
-                        "message": message,
-                        "committer": {
-                            "name": userName,
-                            "email": email
-                        },
-                        "content": decodedValue,
-                        "sha": shaKey
-                    },{headers: {'Authorization': 'token ' + accessToken}}).
-                    then(res1 => {
-                        console.log(res1.data)
+                axios.get('https://api.github.com/repos/' + userName + '/' + project + '/contents/' + fileName, { headers: { 'Authorization': 'token ' + accessToken } }).
+                    then(res => {
+                        let shaKey = res.data.sha;
+                        let decodedValue = Buffer.from(value).toString('base64');
+                        axios.put('https://api.github.com/repos/' + userName + '/' + project + '/contents/' + fileName, {
+                            "message": message,
+                            "committer": {
+                                "name": userName,
+                                "email": email
+                            },
+                            "content": decodedValue,
+                            "sha": shaKey
+                        }, { headers: { 'Authorization': 'token ' + accessToken } }).
+                            then(res1 => {
+                                // this.createNotification('success');
+                                console.log("done !");
+                            }).catch(err => {
+                                this.createNotification('error');
+                            });
                     }).catch(err => {
-                        console.log("Lỗi ở put data");
+                        console.log("Lỗi ở get content");
                     });
-                }).catch(err => {
-                    console.log("Lỗi ở get content");
-                });
             }
         });
     }
+
+
     render() {
         return (
             <div className="SidebarElement">
                 <div className="sidebar_title">GITHUB</div>
-                <div className="sidebar_element" style = {{flexDirection : "column"}}>
+                <div className="sidebar_element" style={{ flexDirection: "column" }}>
                     {!localStorage.uid && <div className="signin_git_button" onClick={() => this.signIn()}>
                         <i className="fab fa-github" style={{ width: '18px', height: '18px', paddingRight: '5px' }}></i>
                         <span>Sign in with GitHub</span>
                     </div>}
-                    <input id="commitMsg" placeholder="Commit messages" style={{ flex: '1', display: 'block' }}></input>
+                    <input id="commitMsg" placeholder="Commit messages" style={{ flex: '1', display: 'block', color : 'black' }}></input>
                     <button onClick={() => this.commitCode()} style={{ background: 'green', cursor: 'pointer' }}>Commit Changes</button>
                 </div>
             </div>
