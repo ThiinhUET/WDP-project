33ed4387dd76e36ef272dbd152dba67d54c43c9f
ThiinhUET <thinhlevan2015@gmail.com>
2019-11-27 07:40:20 +0700

    add modified

--- wdp-client/src/components/layouts/editor/sidebar/Github.js
+++ wdp-client/src/components/layouts/editor/sidebar/Github.js
@@ -4,7 +4,11 @@ import Authenticate from '../../../common/authprovider/Authenticate';
 import { contentFlow } from '../../../../services';
 import { defaultdata } from '../sidebar/explorer/data';
 import axios from 'axios';
-import { Icon, notification} from "antd";
+import { Treebeard, decorators } from 'react-treebeard';
+import defaultStyles from '../sidebar/explorer/defaultStyles';
+import Header from '../sidebar/explorer/Header';
+import Toggle from '../sidebar/explorer/Toggle';
+
 
 
 class Github extends Component {
@@ -15,22 +19,26 @@ class Github extends Component {
             codeToCommit: "",
             data: (location.state && location.state.data) ? location.state.data : defaultdata,
             cursor: (location.state && location.state.cursor) ? location.state.cursor : '',
-        }
+            modifiedTree : []
+        };
+        this.onToggle = this.onToggle.bind(this);
+    
     }
 
     
 
     componentDidMount() {
+        let modified = {...this.state.data};
         this.props.history.listen((location) => this.setState({
             data: (location.state && location.state.data) ? location.state.data : this.state.data,
             cursor: (location.state && location.state.cursor) ? location.state.cursor : this.state.cursor,
         }));
-        console.log(this.state.data);
-        let children = this.state.data.children;
-        for(let i = 0 ; i < children.length; i++){
-            if(children[i].modified === true){
-            }
-        }
+        
+        modified.children =  modified.children.filter((value, index) => {
+             return value.modified === true;
+        });
+
+        this.setState({modifiedTree : modified});
     }
 
     signIn() {
@@ -40,54 +48,68 @@ class Github extends Component {
 
     commitCode() {
         this.contenFlowSub = contentFlow.subscribe((value) => {
-            let userName = localStorage.getItem('username');
-            let email = localStorage.getItem('email');
-            let project = localStorage.getItem('projectName');
-            let accessToken = localStorage.getItem('accessToken');
-            let fileName = this.state.cursor.name;
-            let message = document.getElementById('commitMsg').value;
-            if (value !== '<!--Happy Coding-->') {
-                axios.get('https://api.github.com/repos/' + userName + '/' + project + '/contents/' + fileName, { headers: { 'Authorization': 'token ' + accessToken } }).
-                    then(res => {
-                        let shaKey = res.data.sha;
-                        let decodedValue = Buffer.from(value).toString('base64');
-                        axios.put('https://api.github.com/repos/' + userName + '/' + project + '/contents/' + fileName, {
-                            "message": message,
-                            "committer": {
-                                "name": userName,
-                                "email": email
-                            },
-                            "content": decodedValue,
-                            "sha": shaKey
-                        }, { headers: { 'Authorization': 'token ' + accessToken } }).
-                            then(res1 => {
-                               window.alert("Commit code successfully !")
-                            }).catch(err => {
-                                this.openNotification(false);
-                            });
-                    }).catch(err => {
-                        console.log("Lỗi ở get content");
-                    });
+            try{
+                let userName = localStorage.getItem('username');
+                let email = localStorage.getItem('email');
+                let project = localStorage.getItem('projectName');
+                let accessToken = localStorage.getItem('accessToken');
+                let fileName = this.state.cursor.name;
+                let message = document.getElementById('commitMsg').value;
+                if (value !== '<!--Happy Coding-->') {
+                    axios.get('https://api.github.com/repos/' + userName + '/' + project + '/contents/' + fileName, { headers: { 'Authorization': 'token ' + accessToken } }).
+                        then(res => {
+                            let shaKey = res.data.sha;
+                            let decodedValue = Buffer.from(value).toString('base64');
+                            axios.put('https://api.github.com/repos/' + userName + '/' + project + '/contents/' + fileName, {
+                                "message": message,
+                                "committer": {
+                                    "name": userName,
+                                    "email": email
+                                },
+                                "content": decodedValue,
+                                "sha": shaKey
+                            }, { headers: { 'Authorization': 'token ' + accessToken } }).
+                                then(res1 => {
+                                   window.alert("Commit code successfully !")
+                                }).catch(err => {
+                                    this.openNotification(false);
+                                });
+                        }).catch(err => {
+                            console.log("Lỗi ở get content");
+                        });
+                }
+            }catch(err){
+                console.log(err);
             }
         });
     }
 
-    openNotification = succces => {
-        if (succces)
-          notification.open({
-            message: "Succces",
-            description: "Gửi action thành công",
-            icon: <Icon type="smile" style={{ color: "#108ee9" }} />
-          });
-        else {
-          notification.open({
-            message: "Error",
-            description: "Bị lỗi rồi",
-            icon: <Icon type="smile" rotate={180} style={{ color: "#108ee9" }} />
-          });
+    onToggle(node, toggled) {
+        const { cursor, data } = this.state;
+
+        console.log(cursor);
+        
+        if (cursor) {
+            this.setState(() => ({ cursor, active: false }));
         }
-      };
-    
+
+        node.active = true;
+        if (node.children) {
+            node.toggled = toggled;
+        }
+
+        if (node.type !== 'folder'){
+            this.props.history.push({
+                state: { ...this.props.location.state, cursor: node }
+            });
+        } 
+        this.setState(() => ({ cursor: node, data: Object.assign({}, data) }));
+        // let dataSearch = document.getElementById("searchBox");
+        // if (dataSearch.value) {
+        //     this.setState({data: this.props.location.state.data});
+        //     dataSearch.value = null;
+        // }
+    }
     render() {
         return (
             <div className="SidebarElement">
@@ -99,6 +121,12 @@ class Github extends Component {
                     </div>}
                     <input id="commitMsg" placeholder="Commit messages" style={{ flex: '1', display: 'block', color : 'black', marginBottom: '1vh', textAlign : 'center' }}></input>
                     <button onClick={() => this.commitCode()} style={{ background: 'green', cursor: 'pointer', marginBottom: '1vh'}}>Commit Changes</button>
+                    <Treebeard
+                        style={defaultStyles}
+                        data={this.state.modifiedTree}
+                        onToggle={this.onToggle}
+                        decorators={{ ...decorators, Toggle, Header }}
+                    />
                     <div></div>
                 </div>
             </div>
--- wdp-client/src/components/layouts/editor/sidebar/explorer/Filter.js
+++ wdp-client/src/components/layouts/editor/sidebar/explorer/Filter.js
@@ -5,24 +5,21 @@ import { defaultdata } from './data';
 
 import { dataFlow } from '../../../../../services';
 
-// Helper functions for filtering
 export const defaultMatcher = (filterText, node) => {
     return node.name.toLowerCase().indexOf(filterText.toLowerCase()) !== -1;
 };
 
 export const findNode = (node, filter, matcher) => {
-    return matcher(filter, node) || // i match
-        (node.children && // or i have decendents and one of them match
+    return matcher(filter, node) || 
+        (node.children && 
             node.children.length &&
             !!node.children.find(child => findNode(child, filter, matcher)));
 };
 
 export const filterTree = (node, filter, matcher = defaultMatcher) => {
-    // If im an exact match then all my children get to stay
     if (matcher(filter, node) || !node.children) {
         return node;
     }
-    // If not then only keep the ones that match or have matching descendants
     const filtered = node.children
         .filter(child => findNode(child, filter, matcher))
         .map(child => filterTree(child, filter, matcher));
@@ -36,7 +33,6 @@ export const expandFilteredNodes = (node, filter, matcher = defaultMatcher) => {
     }
     const childrenWithMatches = node.children.filter(child => findNode(child, filter, matcher));
     const shouldExpand = childrenWithMatches.length > 0;
-    // If im going to expand, go through all the matches and see if thier children need to expand
     if (shouldExpand) {
         children = childrenWithMatches.map(child => {
             return expandFilteredNodes(child, filter, matcher);
