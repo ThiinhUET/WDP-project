5669127d319d8a1407014e55f5080f985994dd96
Thanh-Tung Le <lethanhtung.tung06@gmail.com>
2019-12-08 02:08:42 +0700

    update help

--- wdp-client/src/components/pages/help/Help.js
+++ wdp-client/src/components/pages/help/Help.js
@@ -7,10 +7,14 @@ class Help extends Component {
         return (
             <div className="Help">
                 <h1>Help</h1>
-                <span>On the left corner, there are Explorer, Github and Export. </span>
+                <span>On the left side, there are Explorer, Github and Export. </span>
                 <ul>
                     <li>Explorer: There is sample project containing 3 files index.html, script.js, style.css. You can search your file in case your project is large.</li>
+                    <li>Github: You can create new repository here. When you are in Github tab, your coding panel will be splited in two columns which allow you to compare 2 code versions.</li>
+                    <li>Export: You can download you entire project as zip file here.</li>
                 </ul>
+                <span>In the middle, there is your coding editor.</span> <br/> <br/>
+                <span>On the right side, there are your preview and console session. Click to Run whenever you want to compile you code.</span>
             </div>
         );
     }
