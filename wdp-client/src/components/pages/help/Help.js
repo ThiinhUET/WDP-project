0f66daf20893a173a64193566808636f3f31cd63
Thanh-Tung Le <lethanhtung.tung06@gmail.com>
2019-12-08 01:54:36 +0700

    update help

--- wdp-client/src/components/pages/help/Help.js
+++ wdp-client/src/components/pages/help/Help.js
@@ -7,6 +7,10 @@ class Help extends Component {
         return (
             <div className="Help">
                 <h1>Help</h1>
+                <span>On the left corner, there are Explorer, Github and Export. </span>
+                <ul>
+                    <li>Explorer: There is sample project containing 3 files index.html, script.js, style.css. You can search your file in case your project is large.</li>
+                </ul>
             </div>
         );
     }
